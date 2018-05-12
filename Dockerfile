c1531a8ab1e6ae19761141bfbca2d6a805baf5c7
Alexander Mancevice <amancevice@cargometrics.com>
2018-05-12 07:53:53 -0400

    pubsub

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,17 @@
 FROM node:6
+
+# Install emulator
+RUN npm install -g @google-cloud/functions-emulator
+
+# Install app
 ARG PROJECT_ID=slack-drive
+ARG PUBSUB_TOPIC=slack-drive
 ENV PROJECT_ID=${PROJECT_ID} \
-    ENDPOINT=http://localhost:8010/${PROJECT_ID}/us-central1/slackEvent
-RUN npm install -g @google-cloud/functions-emulator && \
-    functions config set projectId ${PROJECT_ID}
+    PUBSUB_TOPIC=${PUBSUB_TOPIC} \
+    GOOGLE_APPLICATION_CREDENTIALS=/etc/cloud/client_secret.json
+VOLUME /etc/cloud \
+       /var/data
 WORKDIR /slack-drive
+COPY config.json index.js package.json ./
+RUN npm install
 CMD ["/bin/bash"]
--- index.js
+++ index.js
@@ -1,9 +1,11 @@
+const PubSub = require('@google-cloud/pubsub');
 const { WebClient } = require('@slack/client');
 const config = require('./config.json');
-const slack = new WebClient(config.slack_api_token);
+const slack = new WebClient(config.slack.api_token);
+const pubsub = new PubSub({projectId: config.cloud.project_id});
 
 /**
- * Bad HTTP Request
+ * Bad HTTP Request.
  */
 class BadRequest extends Error {
   constructor(message) {
@@ -14,7 +16,7 @@ class BadRequest extends Error {
 }
 
 /**
- * Unhandled request or event type
+ * Unhandled request or event type.
  */
 class UnhandledRequest extends Error {
   constructor(message) {
@@ -25,7 +27,7 @@ class UnhandledRequest extends Error {
 }
 
 /**
- * Invalid token
+ * Invalid token.
  */
 class InvalidCredentials extends Error {
   constructor(message) {
@@ -36,127 +38,14 @@ class InvalidCredentials extends Error {
 }
 
 /**
- * Handle channel_created
- *
- * @param {object}  slackEvent                         The Slack event object
- * @param {string}  slackEvent.type                    Event type
- * @param {object}  slackEvent.channel                 Channel object
- * @param {string}  slackEvent.channel.id              Channel ID
- * @param {boolean} slackEvent.channel.is_channel      Is channel
- * @param {string}  slackEvent.channel.name            Channel name
- * @param {string}  slackEvent.channel.name_normalized Normalized channel name
- * @param {number}  slackEvent.channel.created         Created timestamp
- * @param {string}  slackEvent.event_ts                Event timestamp
+ * Publish error.
  */
-function handleChannelCreated(slackEvent) {
-  return slackEvent;
-}
-
-/**
- * Handle channel_rename
- *
- * @param {object}  slackEvent                         The Slack event object
- * @param {string}  slackEvent.type                    Event type
- * @param {object}  slackEvent.channel                 Channel object
- * @param {string}  slackEvent.channel.id              Channel ID
- * @param {boolean} slackEvent.channel.is_channel      Is channel
- * @param {string}  slackEvent.channel.name            Channel name
- * @param {string}  slackEvent.channel.name_normalized Normalized channel name
- * @param {number}  slackEvent.channel.created         Created timestamp
- * @param {string}  slackEvent.event_ts                Event timestamp
- */
-function handleChannelRename(slackEvent) {
-  return slackEvent;
-}
-
-/**
- * Handle group_rename
- *
- * @param {object}  slackEvent                         The Slack event object
- * @param {string}  slackEvent.type                    Event type
- * @param {object}  slackEvent.channel                 Channel object
- * @param {string}  slackEvent.channel.id              Channel ID
- * @param {boolean} slackEvent.channel.is_channel      Is channel
- * @param {string}  slackEvent.channel.name            Channel name
- * @param {string}  slackEvent.channel.name_normalized Normalized channel name
- * @param {number}  slackEvent.channel.created         Created timestamp
- * @param {string}  slackEvent.event_ts                Event timestamp
- */
-function handleGroupRename(slackEvent) {
-  return slackEvent;
-}
-
-/**
- * Handle member_left_channel
- *
- * @param {object} slackEvent              The Slack event object
- * @param {string} slackEvent.type         Event type
- * @param {string} slackEvent.user
- * @param {string} slackEvent.channel
- * @param {string} slackEvent.channel_type Channel type
- * @param {string} slackEvent.team
- * @param {string} slackEvent.event_ts
- */
-function handleMemberLeftChannel(slackEvent) {
-  return slackEvent;
-}
-
-/**
- * Handle member_joined_channel
- *
- * @param {object} slackEvent              The Slack event object
- * @param {string} slackEvent.type         Event type
- * @param {string} slackEvent.user
- * @param {string} slackEvent.channel
- * @param {string} slackEvent.channel_type Channel type
- * @param {string} slackEvent.team
- * @param {string} slackEvent.inviter
- * @param {string} slackEvent.event_ts
- */
-function handleMemberJoinedChannel(slackEvent) {
-  return slackEvent;
-}
-
-/**
- * Handle team_join
- *
- * @param {object} slackEvent      The Slack event object
- * @param {string} slackEvent.type Event type
- * @param {object} slackEvent.user User object
- */
-function handleTeamJoin(slackEvent) {
-  return slackEvent;
-}
-
-/**
- * Handle Slack event types
- *
- * @param {object} slackEvent The Slack event object
- */
-function handleEvent(slackEvent) {
-  return new Promise((resolve, reject) => {
-    if (slackEvent.type === 'channel_created') {
-      resolve(handleChannelCreated(slackEvent));
-    }
-    else if (slackEvent.type === 'channel_rename') {
-      resolve(handleChannelRename(slackEvent));
-    }
-    else if (slackEvent.type === 'group_rename') {
-      resolve(handleGroupRename(slackEvent));
-    }
-    else if (slackEvent.type === 'member_left_channel') {
-      resolve(handleMemberLeftChannel(slackEvent));
-    }
-    else if (slackEvent.type === 'member_joined_channel') {
-      resolve(handleMemberJoinedChannel(slackEvent));
-    }
-    else if (slackEvent.type === 'team_join') {
-      resolve(handleTeamJoin(slackEvent));
-    }
-    else {
-      reject(new UnhandledRequest('Unhandled event type: ' + slackEvent.type));
-    }
-  })
+class PubSubError extends Error {
+  constructor(message) {
+    super(message);
+    this.name = 'PubsubError';
+    this.code = 500;
+  }
 }
 
 /**
@@ -177,35 +66,85 @@ function verifyMethod(method) {
  * @param {string} body.token The Slack token to be verified.
  */
 function verifyToken(body) {
-  if (!body || body.token !== config.slack_verification_token) {
+  if (!body || body.token !== config.slack.verification_token) {
     throw new InvalidCredentials('Invalid Credentials');
   }
 }
 
 /**
- * Compose Slack Message from error
+ * Handle Slack event types.
  *
- * @param {object} error Error object
+ * @param {object} slackEvent The Slack event object.
+ */
+function publishEvent(slackEvent) {
+  let dataBuffer = Buffer.from(JSON.stringify(slackEvent));
+  pubsub.topic(config.cloud.pubsub_topic)
+    .publisher()
+    .publish(dataBuffer)
+    .then((mid) => { return {messageId: mid}; })
+    .catch((err) => { throw new PubSubError(err); });
+  return slackEvent;
+}
+
+/**
+ * Compose Slack message from error.
+ *
+ * @param {object} error Error object.
  */
 function slackErrorMessage(error) {
-  let msg = JSON.parse(JSON.stringify(config.slack_message_template));
+  let msg = JSON.parse(JSON.stringify(config.slack.message_template));
   try {
     msg.attachments[0].fields[0].value = error.name;
     msg.attachments[0].fields[1].value = error.message;
     msg.attachments[0].fields[2].value = '```\n' + error.stack + '\n```';
     msg.attachments[0].ts = new Date()/1000;
   }
-  catch(e) {
+  catch(err) {
     msg.attachments = [
       {
-        color: "danger",
-        text: "```\n" + error + "\n```",
-        footer: "Slack | Google Drive Sync",
+        color: 'danger',
+        text: '```\n' + error + '\n```',
+        footer: 'Slack | Google Drive Sync',
         ts: new Date()/1000
       }
     ];
   }
   return msg;
+}
+
+/**
+ * Compose Slack message from error.
+ *
+ * @param {object} error Error object.
+ */
+function postErrorMessage(error) {
+  slack.chat.postMessage(slackErrorMessage(error))
+    .then((res) => {
+      // Return rejection of original error
+      return Promise.reject(error);
+    })
+    .catch((err) => {
+      // Return rejection of the error to post to Slack
+      return Promise.reject(err);
+    });
+}
+
+/**
+ * Parse Base64-encoded PubSub message.
+ *
+ * @param {object} message           PubSub message.
+ * @param {object} message.data      PubSub message data.
+ * @param {object} message.data.data Base64-encoded message data.
+ */
+function parsePubSubMessage(message) {
+  try {
+    let encodedEvent = Buffer.from(message.data.data, 'base64').toString();
+    return JSON.parse(encodedEvent);
+  }
+  catch(err) {
+    console.error(err);
+    throw new PubSubError('Could not parse Pub/Sub message');
+  }
 }
 
 /**
@@ -215,15 +154,16 @@ function slackErrorMessage(error) {
  * https://[YOUR_REGION].[YOUR_PROJECT_ID].cloudfunctions.net/slackEvent
  *
  * @example
- * curl -X POST "https://us-central1.project-id.cloudfunctions.net/slackEvent" --data @json_filepath
+ * curl -X POST 'https://us-central1.project-id.cloudfunctions.net/slack-event' --data @json_filepath
  *
- * @param {object} request            Cloud Function request object.
- * @param {object} request.body       The request payload.
- * @param {string} request.body.token Slack's verification token.
- * @param {string} request.body.text  The user's search query.
- * @param {object} respose            Cloud Function response object.
+ * @param {object} req            Cloud Function request object.
+ * @param {object} req.body       The request payload.
+ * @param {string} req.body.token Slack's verification token.
+ * @param {string} req.body.type  Slack event type.
+ * @param {string} req.body.event Slack event object.
+ * @param {object} res            Cloud Function response object.
  */
-exports.slackEvent = (req, res) => {
+exports.publishEvent = (req, res) => {
   return Promise.resolve()
     .then(() => {
       // Verify request
@@ -237,25 +177,60 @@ exports.slackEvent = (req, res) => {
 
       // Handle event
       else if (req.body.type === 'event_callback') {
-        return handleEvent(req.body.event);
+        // Log request
+        console.log('Received ' + JSON.stringify(req.body.event));
+        return publishEvent(req.body.event);
       }
 
       // Unknown
       else {
-        throw new UnhandledRequest('Unknown request type: ' + req.body.type);
+        throw new UnhandledRequest('Unknown event type: ' + req.body.type);
       }
     })
-    .then((response) => {
-      res.json(response);
+    .then((msg) => {
+      // Log response
+      console.log('Publishing ' + JSON.stringify(msg));
+
+      // Return JSON
+      res.json(msg);
     })
-    .catch((error) => {
-      slack.chat.postMessage(slackErrorMessage(error))
-      .then((res) => {
-        res.status(error.code || 500).send(error);
-        return Promise.reject(error);
-      }).catch((error) => {
-        res.status(error.code || 500).send(error);
-        return Promise.reject(error);
-      });
+    .catch((err) => {
+      // Log error
+      console.error(err);
+
+      // Post error to Slack
+      res.status(err.code || 500).send(err);
+      return postErrorMessage(err);
     });
+};
+
+/**
+ * Background Cloud Function to be triggered by Pub/Sub.
+ *
+ * @param {object}   event    The Cloud Functions event.
+ * @param {function} callback The callback function.
+ */
+exports.consumeEvent = (message, callback) => {
+  // Parse event JSON
+  let slackEvent = parsePubSubMessage(message);
+
+  // Log event
+  console.log('Consuming ' + JSON.stringify(slackEvent));
+
+  // TODO
+  msg = JSON.parse(JSON.stringify(config.slack.message_template));
+  msg.text = 'A new event has been published'
+  msg.attachments = [
+    {
+      color: 'good',
+      footer: 'Slack | Google Drive',
+      text: '```\n' + JSON.stringify(slackEvent) + '\n```',
+      ts: +new Date()/1000
+    }
+  ];
+  slack.chat.postMessage(msg)
+    .then((res) => {})
+    .catch((err) => {});
+
+  callback();
 };
