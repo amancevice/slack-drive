57d0d1e0b0ca5afb4e8428cc9ecde06cd2951378
Alexander Mancevice <amancevice@cargometrics.com>
2018-05-21 16:41:44 -0400

    build

--- src/event-listener/build.js
+++
@@ -1,3 +0,0 @@
-const pkg = require('./package.json');
-const execSync = require('child_process').execSync;
-execSync(`zip ../../dist/${pkg.name}-${pkg.version}.zip client_secret.json config.json index.js package.json`);
