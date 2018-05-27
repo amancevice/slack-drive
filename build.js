497b85059877454417129a6890e12623b0ce2a5b
Alexander Mancevice <amancevice@cargometrics.com>
2018-05-27 19:53:30 -0400

    build

--- build.js
+++ build.js
@@ -2,8 +2,7 @@ const child_process = require('child_process');
 const packages = ['event-consumer', 'event-publisher', 'redirect', 'slash-command'];
 
 child_process.execSync(`mkdir -p ./dist`);
-packages.map((pkg) => {
-  let cfg = require(`./src/${pkg}/package.json`),
-      files = `client_secret.json config.json index.js messages.json package.json`;
-  child_process.execSync(`cp client_secret.json config.json ./src/${pkg}/ && cd ./src/${pkg} && zip ../../dist/${cfg.name}-${cfg.version}.zip ${files}`);
+packages.map((dir) => {
+  let pkg = require(`./src/${dir}/package.json`);
+  child_process.execSync(`cd ./src/${dir} && zip ../../dist/${pkg.name}-${pkg.version}.zip ./*`);
 });
