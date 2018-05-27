bb830b34e6f4329c1e3505932fbbc5b0671211bf
Alexander Mancevice <amancevice@cargometrics.com>
2018-05-27 19:31:32 -0400

    Remove terraform

--- terraform/cloud/providers.tf
+++
@@ -1,6 +0,0 @@
-provider "google" {
-  credentials = "${var.cloud_credentials}"
-  project     = "${var.cloud_project}"
-  region      = "${var.cloud_region}"
-  version     = "${var.cloud_version}"
-}
