c0877d245748051c55bf66a2fe574d1326b8915f
Alexander Mancevice <amancevice@cargometrics.com>
2018-05-31 11:39:02 -0400

    link files for emulation

--- gulpfile.js
+++ gulpfile.js
@@ -166,6 +166,22 @@ gulp.task('dist', gulp.series([
   'dist-redirect'
 ]));
 
+// Set up links to config.json and client_secret.json
+gulp.task('link', () => {
+  return Promise.all([
+    exec('ln config.json src/event-publisher/config.json'),
+    exec('ln config.json src/event-consumer/config.json'),
+    exec('ln config.json src/slash-command/config.json'),
+    exec('ln config.json src/redirect/config.json'),
+    exec('ln client_secret.json src/event-publisher/client_secret.json'),
+    exec('ln client_secret.json src/event-consumer/client_secret.json'),
+    exec('ln client_secret.json src/redirect/client_secret.json'),
+    exec('ln src/messages.json src/event-consumer/messages.json'),
+    exec('ln src/messages.json src/slash-command/messages.json'),
+    exec('ln src/messages.json src/redirect/messages.json')
+  ]);
+});
+
 // Start functions emulator
 gulp.task('emulator-start', () => {
   return exec(`echo ${process.env.PROJECT_ID} | functions start`);
@@ -183,25 +199,10 @@ gulp.task('emulator-deploy', () => {
 
 // Build docker
 gulp.task('emulator', gulp.series([
+  'link',
   'emulator-start',
   'emulator-deploy'
 ]));
-
-// Set up links to config.json and client_secret.json
-gulp.task('link', () => {
-  return Promise.all([
-    exec('ln config.json src/event-publisher/config.json'),
-    exec('ln config.json src/event-consumer/config.json'),
-    exec('ln config.json src/slash-command/config.json'),
-    exec('ln config.json src/redirect/config.json'),
-    exec('ln client_secret.json src/event-publisher/client_secret.json'),
-    exec('ln client_secret.json src/event-consumer/client_secret.json'),
-    exec('ln client_secret.json src/redirect/client_secret.json'),
-    exec('ln src/messages.json src/event-consumer/messages.json'),
-    exec('ln src/messages.json src/slash-command/messages.json'),
-    exec('ln src/messages.json src/redirect/messages.json')
-  ]);
-});
 
 // Default
 gulp.task('default', gulp.series(['build']));
