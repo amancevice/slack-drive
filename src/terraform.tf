c0e8bba2cf4587f0bd638e2eb117fb84f97160d9
Alexander Mancevice <amancevice@cargometrics.com>
2018-06-02 11:46:25 -0400

    bump version

--- src/terraform.tf
+++ src/terraform.tf
@@ -7,7 +7,7 @@ provider "google" {
 
 module "slack_drive_cloud" {
   source             = "amancevice/slack-drive/google"
-  version            = "0.4.2"
+  version            = "0.4.3"
   bucket_name        = "${var.bucket_name}"
   channel            = "${var.channel}"
   project            = "${var.project}"
